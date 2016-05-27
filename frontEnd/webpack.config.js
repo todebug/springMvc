521ea9218b9a05e5b4127642ee127d6979a43df2
dongyang <todebug@163.com>
2016-05-27 15:53:06 +0800

    edit config

--- frontEnd/webpack.config.js
+++ frontEnd/webpack.config.js
@@ -2,48 +2,60 @@
 var webpack = require('webpack');
 var vue = require('vue-loader');
 module.exports = {
-	entry: {
-		router: './vue/router.js'
-	},
-	output: {
-		path: './dist',
-		publicPath: '/dist/',
-		filename: '[name].js'
-	},
-	module: {
-		loaders: [{
-			test: /\.vue$/,
-			loader: vue.withLoaders({
-				// apply babel transform to all javascript
-				// inside *.vue files.
-				js: 'babel?optional[]=runtime&loose=all'
-			})
-		}]
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
-			$: 'jquery',
-			jQuery: 'jquery',
-			"window.jQuery": "jquery"
-		})
-	]
+    entry: {
+        router: './vue/router.js'
+    },
+    output: {
+        path: './dist',
+        publicPath: '/dist/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+                test: /\.vue$/,
+                loader: vue.withLoaders({
+                    // apply babel transform to all javascript
+                    // inside *.vue files.
+                    js: 'babel?optional[]=runtime&loose=all'
+                })
+            },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192" },
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.(html|tpl)$/, loader: 'html-loader' },
+        ]
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue']
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+            $: 'jquery',
+            jQuery: 'jquery',
+            "window.jQuery": "jquery"
+        })
+    ]
 }
 
 if (process.env.NODE_ENV === 'production') {
-	module.exports.plugins = [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.optimize.OccurenceOrderPlugin()
-	]
+    module.exports.plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ]
 } else {
-	module.exports.devtool = '#source-map'
+    module.exports.devtool = '#source-map'
 }
