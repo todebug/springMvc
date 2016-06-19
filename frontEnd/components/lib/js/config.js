114fc3c92ec131d852217fe823d8163b367fd857
dongyang <todebug@163.com>
2016-06-19 21:26:24 +0800

    update url

--- frontEnd/components/lib/js/config.js
+++ frontEnd/components/lib/js/config.js
@@ -1,7 +1,8 @@
 'use strict';
 //devConfig -host
 var url = {
-    'host': 'http://10.118.22.30:8040/monitor-web/'
+    'host': 'http://1xm5039264.imwork.net:23968/monitor-web/'
+    //'host': 'http://10.118.22.30:8040/monitor-web/'
         //'host': 'http://localhost:8888/'
         //'host': '/monitor-web/'
 };
